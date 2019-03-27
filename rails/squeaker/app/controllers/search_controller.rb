b9394b39717d7f91a71225b42a86387818aecb0d
May <ben.may@ldnmlw-nmx2513.emea.corp.ipgnetwork.com>
2019-03-27 15:21:37 +0000

    Trying to add ajax search

--- rails/squeaker/app/controllers/search_controller.rb
+++ rails/squeaker/app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def show
     @messages = Message.like(params[:query])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @messages }
+    end
   end
 end
