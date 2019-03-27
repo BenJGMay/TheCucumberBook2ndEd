36626a6c5148b9c595114f65129a45d69f86ff63
May <ben.may@mullenloweprofero.com>
2019-03-27 15:34:18 +0000

    Copied over source code to try and get search suggestions working

--- rails/squeaker/app/assets/javascripts/search.js
+++ rails/squeaker/app/assets/javascripts/search.js
@@ -10,6 +10,7 @@ Search.prototype.queue = function (query) {
     self.search(query);
   }, 150);
 };
+
 Search.prototype.search = function (query) {
   var self = this;
   jQuery.ajax({
@@ -21,21 +22,27 @@ Search.prototype.search = function (query) {
     dataType: 'json'
   });
 }
+
 Search.prototype.render = function (results) {
   var html = "";
-  for (var i = 0, l = results.length; i<l; ++1) {
-    var url = '/users/' + results[i].user_id + '/messages' + results[i].id;
+  for (var i = 0, l = results.length; i < l; ++i) {
+    var url = '/users/' + results[i].user_id + '/messages/' + results[i].id;
     html += '<li><a href="' + url + '">' + results[i].content + '</a></li>';
   }
-  jQuery.fn.search = function () {
-    this.each(function () {
-      var search = new Search(this);
-      var input = jQuery(this).find("input[type=text]");
+  jQuery(this.form).find('ol.results').html(html);
+}
 
-      input.bind('keyup', function () {
-        search.queue(this.value);
-      });
+jQuery.fn.search = function () {
+  this.each(function () {
+    var search = new Search(this);
+    var input = jQuery(this).find("input[type=text]");
+
+    input.bind('keyup', function () {
+      search.queue(this.value);
     });
-  };
+  });
+};
+
+jQuery(function() {
   jQuery('#search').search();
 });
